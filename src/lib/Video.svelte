77edf65c577dae776c900d639a5728fb755a18f1
cyric2020 <60243062+cyric2020@users.noreply.github.com>
2023-09-10 22:46:47 +1000

    archive of code in flow's videos

---
+++ src/lib/Video.svelte
@@ -0,0 +1,83 @@
+<script>
+    export let video;
+
+    console.log(video);
+
+    function getTimeAgo(stringDate) {
+        const date = new Date(stringDate);
+        const now = new Date();
+        const diff = now - date;
+
+        const seconds = Math.floor(diff / 1000);
+        const minutes = Math.floor(seconds / 60);
+        const hours = Math.floor(minutes / 60);
+        const days = Math.floor(hours / 24);
+        const weeks = Math.floor(days / 7);
+        const months = Math.floor(days / 30);
+        const years = Math.floor(days / 365);
+
+        if (seconds < 60) {
+            return `${seconds} seconds ago`;
+        } else if (minutes < 60) {
+            return `${minutes} minutes ago`;
+        } else if (hours < 24) {
+            return `${hours} hours ago`;
+        } else if (days < 7) {
+            return `${days} days ago`;
+        } else if (weeks < 4) {
+            return `${weeks} weeks ago`;
+        } else if (months < 12) {
+            return `${months} months ago`;
+        } else {
+            return `${years} years ago`;
+        }
+    }
+</script>
+
+<div class="video">
+    <img src={video.snippet.thumbnails.high.url} alt={video.snippet.title} />
+    <div class="details">
+        <p>{getTimeAgo(video.snippet.publishedAt)}</p>
+        <h4>{video.snippet.title}</h4>
+        <a href="https://www.youtube.com/watch?v={video.id.videoId}" target="_blank">🔗 Watch on YouTube</a>
+    </div>
+</div>
+
+<style>
+    .video {
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 2rem;
+        min-width: 15rem;
+        flex-basis: 0;
+        flex-grow: 1;
+        gap: 0.5rem;
+    }
+
+    .video img {
+        width: 100%;
+        border-radius: 0.5rem;
+        aspect-ratio: 16 / 9;
+        object-fit: cover;
+    }
+
+    .video .details {
+        width: 100%;
+    }
+
+    .video .details h4 {
+        font-weight: 600;
+        margin: 0;
+    }
+
+    .video .details p {
+        margin: 0;
+        font-size: 0.8rem;
+    }
+
+    .video .details a {
+        font-size: 0.8rem;
+        color: #666;
+        text-decoration: none;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { onMount } from 'svelte';
+    import Video from '$lib/Video.svelte';
+    
+    var videos = [];
+
+    onMount(async () => {
+        const res = await fetch('/combined.json');
+        const data = await res.json();
+        console.log(data["data"]);
+        videos = data["data"];
+    });
+</script>
+
+<div class="container">
+    <h1>CodeInFlow Videos</h1>
+
+    <div class="videos">
+        {#each videos as video}
+            <Video {video} />
+        {/each}
+    </div>
+</div>
+
+<style>
+    .container {
+        width: 60%;
+        margin: 2rem auto;
+    }
+
+    .videos {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 1rem;
+    }
+</style>
